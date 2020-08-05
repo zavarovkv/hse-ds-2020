76178bfd3e38180deec6f854a303aa2e3f63988e
Konstantin Zavarov <zavarovkv@gmail.com>
2020-08-05 23:53:45 +0300

    Update bubble_sort.py

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -1,1 +1,17 @@
-afs
+# Python program for implementation of Bubble Sort
+
+
+def BubbleSort(array):
+    n = len(array)
+
+    # Traverse through all array elements
+    for i in range(n - 1):
+
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
